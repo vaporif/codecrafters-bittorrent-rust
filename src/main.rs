292d2bece842decdcc5326083a50a13fd5332c72
Dmitriy Onypko <vaporif@gmail.com>
2023-11-19 15:39:26 +0000

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,30 @@
-use serde_json;
-use std::env;
+use clap::{arg, command, Parser, Subcommand};
+type Chars = Vec<u8>;
 
-// Available if you need it!
-// use serde_bencode
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+#[derive(Parser)]
+#[command(author = "Dmytro Onypko", name = "Torrent Sample Client")]
+struct Cli {
+    #[command(subcommand, name = "action")]
+    command: Command,
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
+#[derive(Subcommand)]
+enum Command {
+    #[command(long_about = "Decode Bencode Value")]
+    Decode {
+        #[arg(
+            name = "bencoded value",
+            help = "value to decode, could be string of non utf8 chars"
+        )]
+        bencoded_value: Chars,
+    },
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Decode { bencoded_value } => {
+            println!("{}", String::from_utf8(bencoded_value).unwrap());
+        }
     }
 }
